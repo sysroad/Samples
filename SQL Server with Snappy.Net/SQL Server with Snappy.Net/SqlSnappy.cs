7ef2da866f7b99a7a0e4c75415761e7c8bb77319
byungil <sysroad@gmail.com>
2019-08-14 16:02:53 +0900

    압축/압축해제 코드 작성

---
+++ SQL Server with Snappy.Net/SQL Server with Snappy.Net/SqlSnappy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlTypes;
+using Snappy;
+
+public class SQLSnappy
+{
+    public static SqlBinary Compress(SqlString srcText)
+    {
+        MemoryStream ms = null;
+
+        try
+        {
+            ms = new MemoryStream();
+
+            using (var ss = new SnappyStream(ms, CompressionMode.Compress))
+            {
+                ms = null;
+
+                var srcBytes = Encoding.UTF8.GetBytes(srcText.Value);
+                ss.Write(srcBytes, 0, srcBytes.Length);
+                ss.Flush();
+                return ms.ToArray();
+            }
+        }
+        catch
+        {
+            return SqlBinary.Null;
+        }
+        finally
+        {
+            ms?.Dispose();
+        }
+    }
+
+    public static SqlString Decompress(SqlBinary srcBinary)
+    {
+        MemoryStream ms = null;
+
+        try
+        {
+            ms = new MemoryStream();
+
+            ms.Write(srcBinary.Value, 0, srcBinary.Length);
+            ms.Flush();
+            ms.Seek(0, SeekOrigin.Begin);
+
+            using (var ss = new SnappyStream(ms, CompressionMode.Decompress))
+            {
+                ms = null;
+
+                var decompress = new List<byte>(srcBinary.Length * 2);
+                var buffer = new byte[1024];
+
+                while (true)
+                {
+                    var nRead = ss.Read(buffer, 0, buffer.Length);
+                    if (nRead > 0)
+                    {
+                        decompress.AddRange(new ArraySegment<byte>(buffer, 0, nRead));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return Encoding.UTF8.GetString(decompress.ToArray());
+            }
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+        finally
+        {
+            ms?.Dispose();
+        }
+    }
+}
